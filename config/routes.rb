7f670963eae2a90d5096ae352e5647991f8e51b5
TCMS31 <TCMS31@users.noreply.github.com>
2023-06-26 16:21:37 -0700

    Transactions::Controller::Implemented fetch & update transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories
-  resources :merchants
+  resources :categories, only: %i[index]
+  resources :merchants, only: %i[index]
+  resources :transactions, only: %i[index show update]
 end
