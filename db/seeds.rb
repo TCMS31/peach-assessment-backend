c0bc84e08c71ce7ebf37535dad2fe54ffddf89da
TCMS31 <TCMS31@users.noreply.github.com>
2023-06-26 18:09:31 -0700

    Transaction::Seed::Added dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require 'database_cleaner'
+require 'csv'
 
 class Seeds
   COLOR_SCALE_GREEN = '#DDF1A3'
@@ -22,12 +23,13 @@ class Seeds
     ["Taxes", "💸", COLOR_SCALE_PURPLE],
   ]
 
-  MERCHANTS  = %w[Uber, United, Chipotle, Payroll, Amazon, Turbo Tax, Blue Cross, AMC, Netflix, Hulu]
+  MERCHANTS  = ['Uber', 'United', 'Chipotle', 'Payroll', 'Amazon', 'Turbo Tax', 'Blue Cross', 'AMC', 'Netflix', 'Hulu']
 
   def update
       clean_db
       create_categories
       create_merchants
+      create_transactions
       Rails.logger.info('Database is seeded')
   end
 
@@ -62,6 +64,28 @@ class Seeds
     end
 
     Rails.logger.info('Added Merchants')
+  end
+
+  def create_transactions
+    transactions_from_csv.each do |transaction|
+      merchant = Merchant.find_by_name(transaction['Merchant'])
+      category = Category.find_by_name(transaction['Category'])
+
+      Transaction.create!(
+        name: transaction['Transaction Name'],
+        amount: transaction['Amount'],
+        date: Date.strptime(transaction['Date'], '%m/%d/%Y'),
+        category_id: category&.id,
+        merchant_id: merchant&.id
+      )
+    end
+
+    Rails.logger.info('Added Transactions')
+  end
+
+  def transactions_from_csv
+    csv_text = File.read(Rails.root.join('transactions.csv'))
+    CSV.parse(csv_text, headers: true, strip: true)
   end
 end
 
